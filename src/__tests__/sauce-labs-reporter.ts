f36e4794235ad20f4e25b8285777a117ed1776d1
Tim Brust <github@timbrust.de>
2019-05-12 21:35:40 +0200

    fix: add missing username arg

--- src/__tests__/sauce-labs-reporter.ts
+++ src/__tests__/sauce-labs-reporter.ts
@@ -5,10 +5,10 @@ class SauceLabsReporter implements jest.Reporter {
 	public onRunComplete(contexts: Set<jest.Context>, results: jest.AggregatedResult) {
 		const passed = results.numFailedTests === 0;
 		if (process.env.CI || process.env.REMOTE) {
-			const user = process.env.SAUCE_ACCESS_KEY!!;
+			const user = process.env.SAUCE_USERNAME!!;
 			const sauceLabsAccount = new SauceLabs({
 				user,
-				key: process.env.SAUCE_USERNAME!!
+				key: process.env.SAUCE_ACCESS_KEY!!
 			});
 			const sessionId = readFileSync('tmp/sessionId', 'utf-8');
 			return sauceLabsAccount.updateJob(user, sessionId, {
