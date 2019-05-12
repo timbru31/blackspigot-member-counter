41f377a76e2a03eb22fbb479e712f23ed2786147
Tim Brust <github@timbrust.de>
2019-05-12 19:51:06 +0200

    chore: update dependencies

--- src/__tests__/sauce-labs-reporter.ts
+++ src/__tests__/sauce-labs-reporter.ts
@@ -1,23 +1,18 @@
 import { readFileSync } from 'fs';
-
-const sauceLabs = require('saucelabs');
+import SauceLabs from 'saucelabs';
 
 class SauceLabsReporter implements jest.Reporter {
 
   public onRunComplete(contexts: Set<jest.Context>, results: jest.AggregatedResult) {
     const passed = results.numFailedTests === 0;
     if (process.env.CI || process.env.REMOTE) {
-      const sauceLabsAccount = new sauceLabs({
-        password: process.env.SAUCE_ACCESS_KEY,
-        username: process.env.SAUCE_USERNAME
+      const sauceLabsAccount = new SauceLabs({
+        user: process.env.SAUCE_ACCESS_KEY,
+        key: process.env.SAUCE_USERNAME
       });
       const sessionId = readFileSync('tmp/sessionId', 'utf-8');
-      sauceLabsAccount.updateJob(sessionId, {
+      return sauceLabsAccount.updateJob(sessionId, {
         passed
-      }, (err: any) => {
-        if (err) {
-          console.log(err);
-        }
       });
     }
   }
