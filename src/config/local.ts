1514909e9f26286241185f753302669c899d0678
Tim Brust <github@timbrust.de>
2020-04-20 01:58:21 +0200

    fix: syntax

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,12 +1,5 @@
-import { Options } from 'webdriverio';
-
-export const localConfig: Options = {
-	capabilities: [
-		{
-			browserName: 'chrome',
-			'goog:chromeOptions': {
-				args: ['headless', 'disable-gpu'],
-			},
-		},
-	],
+export const localConfig: WebDriver.Options = {
+	capabilities: {
+		browserName: 'chrome',
+	},
 };
--- src/config/saucelabs.ts
+++ src/config/saucelabs.ts
@@ -1,19 +1,24 @@
 import { Options } from 'webdriverio';
 
-export interface SauceLabsOptions extends Options {
+export interface SauceLabsOptions extends WebDriver.Options {
 	path: string;
+	key?: string;
+	user?: string;
+	capabilities: SauceLabsCapabilities;
+}
+
+interface SauceLabsCapabilities extends WebDriver.DesiredCapabilities {
+	name?: string;
+	tags?: string[];
 }
 
 export const remoteConfig: SauceLabsOptions = {
-	capabilities: [
-		// tslint:disable-next-line: no-object-literal-type-assertion
-		{
-			browserName: 'chrome',
-			build: process.env.CI ? process.env.TRAVIS_BUILD_ID : undefined,
-			name: `BlackSpigot Test commit ${process.env.CI ? process.env.TRAVIS_COMMIT : 'local'}`,
-			tags: process.env.CI ? [process.env.TRAVIS_BRANCH!] : [],
-		} as WebDriver.DesiredCapabilities,
-	],
+	capabilities: {
+		browserName: 'chrome',
+		build: process.env.CI ? process.env.TRAVIS_BUILD_ID : undefined,
+		name: `BlackSpigot Test commit ${process.env.CI ? process.env.TRAVIS_COMMIT : 'local'}`,
+		tags: process.env.CI ? [process.env.TRAVIS_BRANCH!] : [],
+	},
 	key: process.env.SAUCE_ACCESS_KEY,
 	path: '/wd/hub',
 	user: process.env.SAUCE_USERNAME,
